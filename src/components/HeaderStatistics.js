7e48fdd9780deb6370da43b1c392fd946dd0b35c
kurtlike <gordonfriman2000@gmail.com>
2022-01-18 07:56:05 +0300

    add speed buttons

--- src/components/HeaderStatistics.js
+++ src/components/HeaderStatistics.js
@@ -7,22 +7,69 @@ class HeaderStatistics extends React.Component{
         super(props);
         this.state = {
             vaccinated: 0,
-            regular: 5,
-            infected: 7,
-            died: 0
+            regular: 0,
+            infected: 0,
+            died: 0,
+            speed: 1
         }
+        this.speedUp=this.speedUp.bind(this);
+        this.speedDown=this.speedDown.bind(this);
+        this.stop=this.stop.bind(this);
     }
     componentDidMount() {
         this.interval = setInterval(()=>  getStatistic(this),1000);
     }
+    speedUp(){
+        let speed = this.state.speed + 1;
+        if(speed <= 5){
+            this.setState({
+                speed: speed
+            })
+            speedChange(speed);
+        }
+    }
+    speedDown(){
+        let speed = this.state.speed - 1;
+        if(speed >= 0){
+            this.setState({
+                speed: speed
+            })
+            speedChange(speed);
+        }
+    }
+    stop(){
+        if(this.state.speed !== 0){
+            this.setState({
+                speed: 0
+            })
+            speedChange(0);
+        }
 
+    }
     render() {
-        return (<div id="generalStatistics" className="divContainers textContainers" >
-            <TextContainer text = {this.state.vaccinated} classN = "greenText textContainers"/>
-            <TextContainer text = {this.state.regular} classN = "regularText textContainers"/>
-            <TextContainer text = {this.state.infected} classN = "yellowText textContainers"/>
-            <TextContainer text = {this.state.died} classN = "redText textContainers"/>
-        </div>
+        return (
+            <div id="header" className="divContainers textContainers" >
+                <button className="textContainers" onClick={start}>Начать</button>
+                    <div id="generalStatistics" className="divContainers textContainers">
+                        <TextContainer text = {this.state.vaccinated} classN = "greenText textContainers"/>
+                        <TextContainer text = {this.state.regular} classN = "regularText textContainers"/>
+                        <TextContainer text = {this.state.infected} classN = "yellowText textContainers"/>
+                        <TextContainer text = {this.state.died} classN = "redText textContainers"/>
+                    </div>
+                <div id="speedButtons">
+                    <svg id="left" viewBox="0 0 40 80" xmlns="http://www.w3.org/2000/svg" fill="none" onClick={this.speedDown}>
+                        <path d="M 40 0 L 40 80 L 0 40 Z" stroke="black" strokeWidth={2} fill={"#555151"} opacity={0.2}/>
+                    </svg>
+                    <svg id="centre" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg" fill="none" onClick={this.stop}>
+                        <path d="M 10 10 L 10 70 L 70 70 L 70 10 Z" stroke="black" strokeWidth={2} fill={"#555151"} opacity={0.2}/>
+                        <text x="30" y="50" className="svgText">{this.state.speed}</text>
+                    </svg>
+
+                    <svg id="right" viewBox="0 0 40 80" xmlns="http://www.w3.org/2000/svg" fill="none" onClick={this.speedUp}>
+                        <path d="M 0 0 L 0 80 L 40 40 Z" stroke="black" strokeWidth={2} fill={"#555151"} opacity={0.2}/>
+                    </svg>
+                </div>
+            </div>
         );
     }
 }
@@ -45,3 +92,20 @@ function getStatistic(state) {
             }
         )
 }
+function start() {
+    fetch("http://localhost:8081/startSimulation");
+}
+function speedChange(speed){
+    fetch("http://localhost:8081/changeSpeed", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+           value: speed
+        })
+    });
+}
+
+
